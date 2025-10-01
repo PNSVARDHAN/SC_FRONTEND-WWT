bc7d00140dfafef8b54b70c5907354769e17e309
PNSVARDHAN <pnssvardhan@gmail.com>
2025-10-01 17:27:50 +0530

    updated the vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      '*'  // 
-    ],
-    host: true, 
-    port: 5173
+    host: true,
+    port: 5173,
+    strictPort: true
+  },
+  build: {
+    outDir: 'dist'
   }
-})
+});
+
