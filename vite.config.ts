ed216943f852d1ef849fe882093bfe8caa92b559
PNSVARDHAN <pnssvardhan@gmail.com>
2025-10-01 16:56:44 +0530

    updated the package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: [
-      'd928e88ca296.ngrok-free.app'  // 👈 add your ngrok host here
+      '*'  // 
     ],
-    host: true, // ensure accessible externally
+    host: true, 
     port: 5173
   }
 })
