7349e31dbea3d0a0d47400afaad0c6396963b033
PNSVARDHAN <pnssvardhan@gmail.com>
2025-10-09 08:52:43 +0530

    update the loading

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Lottie from "lottie-react";
-import loadingAnimation from "../../assets/loading.json"; // your Lottie animation
+import loadingAnimation from "../../assets/loading.json"; 
 import "./Profile.css";
 
 interface Device {
